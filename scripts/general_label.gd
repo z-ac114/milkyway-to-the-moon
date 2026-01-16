62e857377c08048bc2c223ad9ce989c5368b1522
Zachary <tlyzachary0411@gmail.com>
2026-01-17 00:00:27 +0800

    updated animations + ui

--- scripts/general_label.gd
+++ scripts/general_label.gd
@@ -1,15 +1,16 @@
-extends Label
-
-@onready var label_2: Label = $"."
-
-func _process(delta):
-	label_2.text = \
-	"Rock: "    + str(Global.f_n(Global.rock))    + "\n" + \
-	"Copper: "  + str(Global.f_n(Global.copper))  + "\n" + \
-	"Iron: "    + str(Global.f_n(Global.iron))    + "\n" + \
-	"Gold: "    + str(Global.f_n(Global.gold))    + "\n" + \
-	"Zinc: "    + str(Global.f_n(Global.zinc))    + "\n" + \
-	"Emerald: " + str(Global.f_n(Global.emerald)) + "\n" + \
-	"Lapis: "   + str(Global.f_n(Global.lapis))   + "\n" + \
-	"Diamond: "   + str(Global.f_n(Global.diamond))   + "\n" + \
-	"Mult: "    + str(Global.f_n(Global.rock1mult))
+extends RichTextLabel
+func _process(_delta: float) -> void:
+	var nbsp = "\u00A0"
+	var line = ""
+	
+	line += "[img=40]res://assets/rockk.png[/img]%s%s    " % [nbsp, Global.f_n(Global.rock)]
+	line += "\n[img=40]res://assets/copperingot.png[/img]%s%s    " % [nbsp, Global.f_n(Global.copper)]
+	line += "\n[img=40]res://assets/ironingot.png[/img]%s%s    " % [nbsp, Global.f_n(Global.iron)]
+	line += "\n[img=40]res://assets/goldingot.png[/img]%s%s    " % [nbsp, Global.f_n(Global.gold)]
+	line += "\n[img=40]res://assets/zincingot.png[/img]%s%s    " % [nbsp, Global.f_n(Global.zinc)]
+	line += "\n[img=40]res://assets/emeraldd.png[/img]%s%s    " % [nbsp, Global.f_n(Global.emerald)]
+	line += "\n[img=40]res://assets/lapislazuli.png[/img]%s%s    " % [nbsp, Global.f_n(Global.lapis)]
+	line += "\n[img=40]res://assets/diamondd.png[/img]%s%s" % [nbsp, Global.f_n(Global.diamond)]
+	line += "\nMult: "    + str(Global.f_n(Global.rock1mult))
+	
+	text = line
--- scripts/rocket.gd
+++ scripts/rocket.gd
@@ -17,13 +17,13 @@ extends Node2D
 @onready var tank_1: TextureRect = $Tank1
 @onready var tank_2: TextureRect = $Tank2
 @onready var top_cone_1: TextureRect = $TopCone1
-@onready var rocket: TextureRect = $Rocket
-@onready var rocket_2: TextureRect = $Rocket2
 @onready var plating_cost: RichTextLabel = $PlatingCost
 @onready var engine_cost: RichTextLabel = $EngineCost
 @onready var fins_cost: RichTextLabel = $FinsCost
 @onready var tank_cost: RichTextLabel = $TankCost
 @onready var topcone_cost: RichTextLabel = $TopconeCost
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var rocket: TextureRect = $Rocket
 
 var rocket_parts = {
 	"plating": {"costs":[{"copper":10},{"iron":15,"copper":10},{"gold":20,"iron":15},{"zinc":25,"gold":20},{"emerald":30,"zinc":25},{"lapis":35,"emerald":30},{"diamond":40,"lapis":35}]},
@@ -33,82 +33,20 @@ var rocket_parts = {
 	"tank": {"costs":[{"copper":32},{"iron":44,"copper":32},{"gold":56,"iron":44},{"zinc":68,"gold":56},{"emerald":80,"zinc":68},{"lapis":92,"emerald":80},{"diamond":104,"lapis":92}]}
 }
 
-var plating_textures = [
-	preload("res://assets/steelplate.png"),
-	preload("res://assets/copperplate.png"),
-	preload("res://assets/ironplate.png"),
-	preload("res://assets/goldplate.png"),
-	preload("res://assets/zincplate.png"),
-	preload("res://assets/emeraldplate.png"),
-	preload("res://assets/lapisplate.png"),
-	preload("res://assets/diamondplate.png")
-]
-
-var fins_textures = [
-	preload("res://assets/steelfin.png"),
-	preload("res://assets/copperfin.png"),
-	preload("res://assets/ironfin.png"),
-	preload("res://assets/goldfin.png"),
-	preload("res://assets/zincfin.png"),
-	preload("res://assets/emeraldfin.png"),
-	preload("res://assets/lapisfin.png"),
-	preload("res://assets/diamondfin.png")
-]
-
-var engine_textures = [
-	preload("res://assets/steelengine.png"),
-	preload("res://assets/copperengine.png"),
-	preload("res://assets/ironengine.png"),
-	preload("res://assets/goldengine.png"),
-	preload("res://assets/zincengine.png"),
-	preload("res://assets/emeraldengine.png"),
-	preload("res://assets/lapisengine.png"),
-	preload("res://assets/diamondengine.png")
-]
-
-var tank_textures = [
-	preload("res://assets/steeltank.png"),
-	preload("res://assets/coppertank.png"),
-	preload("res://assets/irontank.png"),
-	preload("res://assets/goldtank.png"),
-	preload("res://assets/zinctank.png"),
-	preload("res://assets/emeraldtank.png"),
-	preload("res://assets/lapistank.png"),
-	preload("res://assets/diamondtank.png")
-]
-
-var top_cone_textures = [
-	preload("res://assets/steeltopcone.png"),
-	preload("res://assets/coppertopcone.png"),
-	preload("res://assets/irontopcone.png"),
-	preload("res://assets/goldtopcone.png"),
-	preload("res://assets/zinctopcone.png"),
-	preload("res://assets/emeraldtopcone.png"),
-	preload("res://assets/lapistopcone.png"),
-	preload("res://assets/diamondtopcone.png")
-]
-
-var rocket_textures = [
-	preload("res://assets/steelrocket.png"),
-	preload("res://assets/copperrocket.png"),
-	preload("res://assets/ironrocket.png"),
-	preload("res://assets/goldrocket.png"),
-	preload("res://assets/zincrocket.png"),
-	preload("res://assets/emeraldrocket.png"),
-	preload("res://assets/lapisrocket.png"),
-	preload("res://assets/diamondrocket.png")
-]
-
-
-var original_positions := {}
+var plating_textures = [preload("res://assets/steelplate.png"), preload("res://assets/copperplate.png"), preload("res://assets/ironplate.png"), preload("res://assets/goldplate.png"), preload("res://assets/zincplate.png"), preload("res://assets/emeraldplate.png"), preload("res://assets/lapisplate.png"), preload("res://assets/diamondplate.png")]
+var fins_textures = [preload("res://assets/steelfin.png"), preload("res://assets/copperfin.png"), preload("res://assets/ironfin.png"), preload("res://assets/goldfin.png"), preload("res://assets/zincfin.png"), preload("res://assets/emeraldfin.png"), preload("res://assets/lapisfin.png"), preload("res://assets/diamondfin.png")]
+var engine_textures = [preload("res://assets/steelengine.png"), preload("res://assets/copperengine.png"), preload("res://assets/ironengine.png"), preload("res://assets/goldengine.png"), preload("res://assets/zincengine.png"), preload("res://assets/emeraldengine.png"), preload("res://assets/lapisengine.png"), preload("res://assets/diamondengine.png")]
+var tank_textures = [preload("res://assets/steeltank.png"), preload("res://assets/coppertank.png"), preload("res://assets/irontank.png"), preload("res://assets/goldtank.png"), preload("res://assets/zinctank.png"), preload("res://assets/emeraldtank.png"), preload("res://assets/lapistank.png"), preload("res://assets/diamondtank.png")]
+var top_cone_textures = [preload("res://assets/steeltopcone.png"), preload("res://assets/coppertopcone.png"), preload("res://assets/irontopcone.png"), preload("res://assets/goldtopcone.png"), preload("res://assets/zinctopcone.png"), preload("res://assets/emeraldtopcone.png"), preload("res://assets/lapistopcone.png"), preload("res://assets/diamondtopcone.png")]
+var rocket_textures = [preload("res://assets/steelrocket.png"), preload("res://assets/copperrocket.png"), preload("res://assets/ironrocket.png"), preload("res://assets/goldrocket.png"), preload("res://assets/zincrocket.png"), preload("res://assets/emeraldrocket.png"), preload("res://assets/lapisrocket.png"), preload("res://assets/diamondrocket.png")]
 
 func _ready() -> void:
-	upgrade_plating_button.connect("pressed", Callable(self, "_on_upgrade_plating_pressed"))
-	upgrade_engine_button.connect("pressed", Callable(self, "_on_upgrade_engine_pressed"))
-	upgrade_fins_button.connect("pressed", Callable(self, "_on_upgrade_fins_pressed"))
-	upgrade_topcone_button.connect("pressed", Callable(self, "_on_upgrade_topcone_pressed"))
-	upgrade_tank_button.connect("pressed", Callable(self, "_on_upgrade_tank_pressed"))
-	craft_button.connect("pressed", Callable(self, "_on_craft_pressed"))
+	upgrade_plating_button.pressed.connect(_on_upgrade_plating_pressed)
+	upgrade_engine_button.pressed.connect(_on_upgrade_engine_pressed)
+	upgrade_fins_button.pressed.connect(_on_upgrade_fins_pressed)
+	upgrade_topcone_button.pressed.connect(_on_upgrade_topcone_pressed)
+	upgrade_tank_button.pressed.connect(_on_upgrade_tank_pressed)
+	craft_button.pressed.connect(_on_craft_pressed)
 	_update_textures()
 	rocket.visible = false
 
@@ -122,7 +60,7 @@ func _update_textures():
 	tank_2.texture = tank_textures[Global.rocket_levels["tank"]]
 	top_cone_1.texture = top_cone_textures[Global.rocket_levels["topcone"]]
 	_update_cost_display()
-	
+
 func _update_cost_display():
 	update_specific_cost("plating", plating_cost)
 	update_specific_cost("engine", engine_cost) 
@@ -138,12 +76,9 @@ func update_specific_cost(part_name: String, label_node: RichTextLabel):
 	if level < costs_list.size():
 		var next_cost = costs_list[level]
 		var cost_string = "Cost: "
-		
 		for material in next_cost.keys():
 			var amount = next_cost[material]
 			var icon_path = ""
-			
-			# Identify the icon
 			match material:
 				"rock": icon_path = "res://assets/rockk.png"
 				"copper": icon_path = "res://assets/copperingot.png"
@@ -154,142 +89,72 @@ func update_specific_cost(part_name: String, label_node: RichTextLabel):
 				"lapis": icon_path = "res://assets/lapislazuli.png"
 				"diamond": icon_path = "res://assets/diamondd.png"
 
-			var player_has = Global.get(material)
-			var color_tag = ""
-			var end_tag = ""
-			
-			if player_has < amount:
-				color_tag = "[color=red]"
-				end_tag = "[/color]"
-			
+			var color_tag = "[color=red]" if Global.get(material) < amount else ""
+			var end_tag = "[/color]" if color_tag != "" else ""
 			cost_string += "[img=24]%s[/img]%s%s%s%s  " % [icon_path, nbsp, color_tag, str(amount), end_tag]
-		
 		label_node.text = cost_string
 	else:
 		label_node.text = part_name.capitalize() + ": [color=#FFD700]MAX[/color]"
 
-func _on_back_button_pressed() -> void:
-	Sfxmanager.play_button_click()
-	get_tree().change_scene_to_file("res://scenes/s1.tscn")
-	_finish_crafting()
-	
-func _on_launch_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/rocketlaunch.tscn")
-	_finish_crafting()
-
-func _on_upgrade_plating_pressed() -> void: upgrade_part("plating")
-func _on_upgrade_engine_pressed() -> void: upgrade_part("engine")
-func _on_upgrade_fins_pressed() -> void: upgrade_part("fins")
-func _on_upgrade_topcone_pressed() -> void: upgrade_part("topcone")
-func _on_upgrade_tank_pressed() -> void: upgrade_part("tank")
-func _on_craft_pressed() -> void: craft_rocket()
-
 func upgrade_part(part_name: String):
 	var level = Global.rocket_levels[part_name]
 	var costs = rocket_parts[part_name]["costs"]
-
-	if level >= costs.size():
-		print(part_name, " is fully upgraded!")
-		return
+	if level >= costs.size(): return
 
 	var cost = costs[level]
 	if can_afford(cost):
 		deduct_cost(cost)
 		Global.rocket_levels[part_name] += 1
 		_update_textures()
-		print("Upgraded ", part_name, " to level ", Global.rocket_levels[part_name])
-	else:
-		print("Not enough resources to upgrade ", part_name)
 
 func can_afford(cost: Dictionary) -> bool:
 	for material in cost.keys():
-		if Global.get(material) < cost[material]:
-			return false
+		if Global.get(material) < cost[material]: return false
 	return true
 
 func deduct_cost(cost: Dictionary):
 	for material in cost.keys():
 		Global.set(material, Global.get(material) - cost[material])
 
-func craft_rocket():
-	var levels = []
-	for part_name in rocket_parts.keys():
-		levels.append(Global.rocket_levels[part_name])
-
+func _on_craft_pressed() -> void:
+	var levels = rocket_parts.keys().map(func(p): return Global.rocket_levels[p])
 	var all_same = levels.all(func(l): return l == levels[0])
-	var minimum_level = 1
-	var all_minimum = levels.all(func(l): return l >= minimum_level)
-
-	if all_same and all_minimum:
-		print("Rocket crafted successfully with level: " + str(Global.rocket_levels["plating"]))
-		rocket.texture = rocket_textures[Global.rocket_levels["plating"]]
-		Global.rocket_inventory.append(Global.rocket_levels["plating"])
-		craft_button.disabled = true
-		start_crafting_animation()
-	else:
-		print("Failed to craft - all parts must be at the same level (minimum level 1)")
-
-func start_crafting_animation():
-	var parts = [
-		plating_1, plating_2,
-		fins_1, fins_2,
-		engine_1,
-		tank_1, tank_2,
-		top_cone_1
-	]
 	
-	for p in parts:
-		original_positions[p] = p.global_position
+	if all_same and levels[0] > 0:
+		start_crafting_animation(levels[0])
 
-	var center = rocket_2.global_position
-	var delay_step = 0.2
-	var start_radius = 150.0
-	var duration = 2.0
-
-	for i in range(parts.size()):
-		var part = parts[i]
-		var angle = (TAU / parts.size()) * i
-		var tween = create_tween()
-
-		tween.tween_method(
-			func(new_radius):
-				var spin_angle = angle + (Time.get_ticks_msec() / 200.0)
-				var pos = center + Vector2(cos(spin_angle), sin(spin_angle)) * new_radius
-				part.global_position = pos, start_radius, 0.0, duration).set_delay(i * delay_step)
-
-		tween.tween_callback(Callable(self, "_on_part_animation_finished").bind(part)).set_delay(i * delay_step + duration)
-
-func _on_part_animation_finished(part: TextureRect):
-	part.visible = false
-
-	var all_invisible = true
-	for p in original_positions.keys():
-		if p.visible:
-			all_invisible = false
-			break
+func start_crafting_animation(level: int):
+	rocket.texture = rocket_textures[level]
+	Global.rocket_inventory.append(level)
+	craft_button.disabled = true
+	animation_player.play("craft_rocket")
+	rocket.show()
 	
-	if all_invisible:
-		rocket.visible = true
-		await get_tree().create_timer(1.0).timeout
-		rocket.visible = false
-		for part_name in rocket_parts.keys():
-			Global.rocket_levels[part_name] = 0
-		_update_textures()
-		for p in original_positions.keys():
-			p.global_position = original_positions[p]
-			p.visible = true
-			p.modulate.a = 1.0
-		original_positions.clear()
-		craft_button.disabled = false
+	await animation_player.animation_finished
+	_finish_crafting()
 
 func _finish_crafting():
-	rocket.visible = false
 	for part_name in rocket_parts.keys():
 		Global.rocket_levels[part_name] = 0
+	
 	_update_textures()
-	for p in original_positions.keys():
-		p.global_position = original_positions[p]
+	rocket.visible = false
+	craft_button.disabled = false
+	
+	var parts = [plating_1, plating_2, fins_1, fins_2, engine_1, tank_1, tank_2, top_cone_1]
+	for p in parts:
 		p.visible = true
 		p.modulate.a = 1.0
-	original_positions.clear()
-	craft_button.disabled = false
+
+func _on_back_button_pressed() -> void:
+	Sfxmanager.play_button_click()
+	get_tree().change_scene_to_file("res://scenes/s1.tscn")
+
+func _on_launch_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/rocketlaunch.tscn")
+
+func _on_upgrade_plating_pressed(): upgrade_part("plating")
+func _on_upgrade_engine_pressed(): upgrade_part("engine")
+func _on_upgrade_fins_pressed(): upgrade_part("fins")
+func _on_upgrade_topcone_pressed(): upgrade_part("topcone")
+func _on_upgrade_tank_pressed(): upgrade_part("tank")
